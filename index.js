d2bbf5c8da6f29276749a3b90bf853d91af332fe
Trigun Joshi <trigunjoshi@gmail.com>
2022-11-06 12:29:32 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8008;
+
+app.get('/', (req, res) => {
+  res.send({
+    hi: 'there'
+  })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+})
